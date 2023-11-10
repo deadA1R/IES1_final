48a07f7f89bf8cd9e6553dec9502bd4223aa5bf7
Ilya Rogov <ilya.r.2003@mail.ru>
2023-11-10 15:33:31 +0600

    v0.1.1 add 8 led

--- final_project.c
+++ final_project.c
@@ -6,47 +6,56 @@ void main (void){
 	switch(SW){
 		case(0):
 		{
-			P2 = 0x03;
+			P0 = 0x03;
+			P2 = 0x00;
 			break;
 		}
 		case(1):
 		{
-			P2 = 0x9F;
+			P0 = 0x9F;
+			P2 = 0x01;
 			break;
 		}
 		case(2):
 		{
-			P2 = 0x25;
+			P0 = 0x25;
+			P2 = 0x02;
 			break;
 		}
 		case(3):
 		{
-			P2 = 0x0D;
+			P0 = 0x0D;
+			P2 = 0x04;
 			break;
 		}
 		case(4):
 		{
-			P2 = 0x99;
+			P0 = 0x99;
+			P2 = 0x08;
 			break;
 		}
 		case(5):
 		{
-			P2 = 0x49;
+			P0 = 0x49;
+			P2 = 0x10;
 			break;
 		}
 		case(6):
 		{
-			P2 = 0x41;
+			P0 = 0x41;
+			P2 = 0x20;
 			break;
 		}
 		case(7):
 		{
-			P2 = 0x1F;
+			P0 = 0x1F;
+			P2 = 0x40;
 			break;
 		}
 		case(8):
 		{
-			P2 = 0x01;
+			P0 = 0x01;
+			P2 = 0x80;
 			break;
 		}
 	
