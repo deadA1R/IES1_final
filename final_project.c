3a06321ef0bd8243c5f91b07b35a8c863585b3f7
Ilya Rogov <ilya.r.2003@mail.ru>
2023-11-12 02:58:48 +0600

    v1.0.1 without comments

--- final_project.c
+++ final_project.c
@@ -1,65 +1,83 @@
 #include <reg51.h>
 
-void main (void){
-	unsigned char SW;
-	SW =P1;
+unsigned char floor; 
+unsigned char targetFloor = 0; 
+unsigned char callButtons = 0; 
 
-	switch(SW){
-		case(0):
-		{
-			P0 = 0xC0;
-			P2 = 0x00;
-			break;
-		}
-		case(1):
-		{
-			P0 = 0xF9;
-			P2 = 0x01;
-			break;
-		}
-		case(2):
-		{
-			P0 = 0xA4;
-			P2 = 0x02;
-			break;
-		}
-		case(4):
-		{
-			P0 = 0xB0;
-			P2 = 0x04;
-			break;
-		}
-		case(8):
-		{
-			P0 = 0x99;
-			P2 = 0x08;
-			break;
-		}
-		case(16):
-		{
-			P0 = 0x92;
-			P2 = 0x10;
-			break;
-		}
-		case(32):
-		{
-			P0 = 0x82;
-			P2 = 0x20;
-			break;
-		}
-		case(64):
-		{
-			P0 = 0xF8;
-			P2 = 0x40;
-			break;
-		}
-		case(128):
-		{
-			P0 = 0x80;
-			P2 = 0x80;
-			break;
-		}
-	
-	}
-		
+unsigned char displayValues[] = {0xC0, 0xF9, 0xA4, 0xB0, 0x99, 0x92, 0x82, 0xF8, 0x80};
+
+
+void moveElevator() {
+    if (floor < targetFloor) {
+        floor++;
+    } else if (floor > targetFloor) {
+        floor--;
+    }
+    targetFloor = floor; 
+}
+
+void checkCallButtons() {
+    unsigned char i;
+    for (i = 0; i < 8; i++) {
+        if ((callButtons & (1 << i)) != 0) {
+            targetFloor = i;
+            break;
+        }
+    }
+}
+
+void main (void) {
+    unsigned char SW;
+    while(1) {
+        SW = P1;
+        callButtons = P3;
+        if (floor == targetFloor) {
+            checkCallButtons();
+            switch(SW) {
+                case(1):
+                {
+                    targetFloor = 0;
+                    break;
+                }
+                case(2):
+                {
+                    targetFloor = 1;
+                    break;
+                }
+                case(4):
+                {
+                    targetFloor = 2;
+                    break;
+                }
+                case(8):
+                {
+                    targetFloor = 3;
+                    break;
+                }
+                case(16):
+                {
+                    targetFloor = 4;
+                    break;
+                }
+                case(32):
+                {
+                    targetFloor = 5;
+                    break;
+                }
+                case(64):
+                {
+                    targetFloor = 6;
+                    break;
+                }
+                case(128):
+                {
+                    targetFloor = 7;
+                    break;
+                }
+            }
+        }
+        moveElevator();
+        P0 = displayValues[floor + 1]; 	
+        P2 = 1 << floor; 
+    }
 }
